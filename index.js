83948ac8d81bd8b6b1592c7680c6602f3c6989a6
Nomsku <albin.sihvola@metropolia.fi>
2023-09-14 14:50:58 +0300

    chat

--- index.js
+++ index.js
@@ -4,19 +4,54 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const users = [];
 
 app.use(express.static('public'));
+
+const getUserList = () => {
+    let userList = '';
+    for (const user of users) {
+        userList += user.username + '\n';
+    }
+    return userList;
+};
+const getUserName = (socketId) => {
+    return users.find(user => user.id === socketId)?.username;
+};
+const deleteUserById = (socketId) => {
+    try {
+        const index = users.findIndex(user => user.id === socketId);
+        console.log('deleting user', users[index]);
+        users.splice(index, 1);
+    }catch (error) {
+        console.warn('deleting user failed', error);
+    }
+};
+
+
 
 io.on('connection', (socket) => {
   console.log('a user connected', socket.id);
 
+  socket.on('join', (username) => {
+    users.push({username: username, id: socket.id});
+    console.log('users connected:', users);
+    socket.emit('response', 'Joined with username ' + username);
+    socket.emit('chat message', `Hello ${username}! Users online: ${getUserList()}`);
+  });
+
   socket.on('disconnect', () => {
     console.log('a user disconnected', socket.id);
+    deleteUserById(socket.id);
   });
 
   socket.on('chat message', (msg) => {
-    console.log('message: ', msg);
-    io.emit('chat message', msg);
+    console.log('message by: ' + socket.id, msg);
+    if (msg === '!users'){
+        socket.emit('chat message', `Users online: ${getUserList()}`);
+    } else {
+    io.emit('chat message', `${getUserName(socket.id)}: ${msg}`);
+    }
   });
 });
 
--- public/chat.js
+++ public/chat.js
@@ -3,12 +3,31 @@
 // Server URL below must point to your server, localhost works for local development/testing
 const socket = io('http://localhost:3000');
 
-document.querySelector('form').addEventListener('submit', (event) => {
-  event.preventDefault();
-  const inp = document.getElementById('m');
-  socket.emit('chat message', inp.value);
-  inp.value = '';
-});
+const messages = document.getElementById('messages');
+const msgForm = document.getElementById('input-form');
+const msgInput = msgForm.getElementsByTagName('input')[0];
+const joinForm = document.getElementById('join-form');
+const usernameInput = joinForm.getElementsByTagName('input')[0];
+document.getElementById('chat').classList = 'hidden';
+
+msgForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (msgInput.value) {
+      socket.emit('chat message', msgInput.value);
+      msgInput.value = '';
+    }
+  });
+  
+  joinForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (usernameInput.value) {
+      socket.emit('join', usernameInput.value);
+      usernameInput.value = '';
+      document.getElementById('login').classList = 'hidden';
+      document.getElementById('chat').classList = '';
+      msgInput.focus();
+    }
+  });
 
 socket.on('chat message', (msg) => {
   const item = document.createElement('li');
